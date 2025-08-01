19be421085d39c993a7d6909193ced643b669078
Rahul Pal <rpal730@gmail.com>
2025-08-01 22:43:29 +0530

    _

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,6 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Hello World')));
+    return const Scaffold(body: Center(child: Text('Hello World 2')));
   }
 }
